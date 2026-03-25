f2c48cc9ffdd0c0ee6196274cf3e7cff3f02b2e0
o.kombarov <o.kombarov@g.nsu.ru>
2026-03-26 05:10:41 +0700

    +amdal

--- lab1/src/timed-type-1.cpp
+++ lab1/src/timed-type-1.cpp
@@ -6,7 +6,6 @@
 
 using namespace std;
 
-
 int getLocalRows(int N, int size, int rank) {
     int base = N / size;
     int rem = N % size;
@@ -44,7 +43,7 @@ void fillLocalMat(vector<double> &localA, int N, int localRows, int rowStart) {
 
 double vectorNorm(const vector<double> &v) {
     double sum = 0.0;
-    for (double value: v) {
+    for (double value : v) {
         sum += value * value;
     }
     return sqrt(sum);
@@ -74,7 +73,8 @@ vector<double> simpleIterationMethodMPIv1(
     double tau,
     double epsilon,
     int maxIterations,
-    MPI_Comm comm
+    MPI_Comm comm,
+    double &seqTime
 ) {
     int rank, size;
     MPI_Comm_rank(comm, &rank);
@@ -86,18 +86,23 @@ vector<double> simpleIterationMethodMPIv1(
     vector<double> x(N, 0.0);
     vector<double> xLocalNew(localRows, 0.0);
 
+    // Малый последовательный участок: вычисление нормы b
+    double tSeqStart = MPI_Wtime();
     double normB = vectorNorm(b);
     if (normB == 0.0) {
         normB = 1.0;
     }
+    seqTime += MPI_Wtime() - tSeqStart;
 
     vector<double> localAx(localRows);
 
     for (int iter = 0; iter < maxIterations; ++iter) {
+        // Параллельная вычислительная часть
         multiplyLocalMatrixVector(localA, x, localAx, localRows, N);
 
         double localResidualSum = 0.0;
 
+        // Параллельная вычислительная часть
         for (int i = 0; i < localRows; ++i) {
             int globalRow = rowStart + i;
             double residual = localAx[i] - b[globalRow];
@@ -107,7 +112,8 @@ vector<double> simpleIterationMethodMPIv1(
         }
 
         double globalResidualSum = 0.0;
-        //раздает всем сумму
+
+        // MPI-коммуникация
         MPI_Allreduce(
             &localResidualSum,
             &globalResidualSum,
@@ -117,9 +123,14 @@ vector<double> simpleIterationMethodMPIv1(
             comm
         );
 
+        // Малый последовательный участок:
+        // вычисление критерия и проверка условия остановки
+        tSeqStart = MPI_Wtime();
         double criterion = sqrt(globalResidualSum) / normB;
+        bool converged = (criterion < epsilon);
+        seqTime += MPI_Wtime() - tSeqStart;
 
-        //раздать полный массив всем, v учитывает разные размеры массивов
+        // MPI-коммуникация
         MPI_Allgatherv(
             xLocalNew.data(),
             localRows,
@@ -131,16 +142,21 @@ vector<double> simpleIterationMethodMPIv1(
             comm
         );
 
-        if (criterion < epsilon) {
+        if (converged) {
             if (rank == 0) {
+                // Малый последовательный участок на rank 0: вывод
+                tSeqStart = MPI_Wtime();
                 cout << "Converged in " << iter << " iterations.\n";
+                seqTime += MPI_Wtime() - tSeqStart;
             }
             return x;
         }
     }
 
     if (rank == 0) {
+        double tSeqStart = MPI_Wtime();
         cout << "Maximum number of iterations reached.\n";
+        seqTime += MPI_Wtime() - tSeqStart;
     }
 
     return x;
@@ -162,10 +178,14 @@ int main(int argc, char *argv[]) {
     int rowStart = getRowStart(N, size, rank);
 
     vector<double> localA(localRows * N);
+
+    // Формально это локальная инициализация, не MPI и не "чистая последовательная часть алгоритма".
+    // Но если хочешь, можно НЕ включать её в seqTime.
     fillLocalMat(localA, N, localRows, rowStart);
 
     vector<double> b(N, static_cast<double>(N + 1));
 
+    double seqTime = 0.0;
     double startTime = MPI_Wtime();
 
     vector<double> x = simpleIterationMethodMPIv1(
@@ -177,19 +197,31 @@ int main(int argc, char *argv[]) {
         tau,
         epsilon,
         maxIterations,
-        MPI_COMM_WORLD
+        MPI_COMM_WORLD,
+        seqTime
     );
 
     double endTime = MPI_Wtime();
+    double totalTime = endTime - startTime;
 
     auto max_diff = 0.0;
-    for (auto xi: x) {
+    for (auto xi : x) {
         max_diff = max(max_diff, abs(1.0 - xi));
     }
 
+    // Для корректной оценки берём максимум по процессам
+    double maxTotalTime = 0.0;
+    double maxSeqTime = 0.0;
+
+    MPI_Reduce(&totalTime, &maxTotalTime, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&seqTime, &maxSeqTime, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
     if (rank == 0) {
         cout << fixed << setprecision(10);
-        cout << "Elapsed time: " << (endTime - startTime) << " sec\n";
+        cout << "Elapsed time: " << maxTotalTime << " sec\n";
+        cout << "Estimated tiny sequential time: " << maxSeqTime << " sec\n";
+        cout << "Estimated tiny sequential fraction: "
+             << (maxSeqTime / maxTotalTime) * 100.0 << " %\n";
         cout << "max_diff = " << max_diff << "\n";
         cout << "good answer? " << ((max_diff < epsilon) ? "Yes" : "No") << "\n";
     }
